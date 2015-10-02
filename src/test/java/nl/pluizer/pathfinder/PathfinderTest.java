14eb423fca9383beec7c68d6f9b3ea0cc7f3e0fc
pluizer <pluizer@freeshell.de>
2015-10-02 16:21:22 +0200

    fixed test

--- src/test/java/nl/pluizer/pathfinder/PathfinderTest.java
+++ src/test/java/nl/pluizer/pathfinder/PathfinderTest.java
@@ -1,14 +1,18 @@
 package nl.pluizer.pathfinder;
 
-import org.junit.Test;
+import junit.framework.TestCase;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PathfinderTest {
+/**
+ * Created by pluizer on 02/10/15.
+ */
+public class PathfinderTest extends TestCase {
 
-    @Test
     public void testFindPath() throws Exception {
+
+        // Find a path in a square world:
         World world = new SquareWorld(7, 5);
         world.get(2, 1).setIsReachable(false);
         world.get(3, 1).setIsReachable(false);
@@ -16,10 +20,8 @@ public class PathfinderTest {
         world.get(3, 3).setIsReachable(false);
         world.get(4, 3).setIsReachable(false);
         List<Node> path = Pathfinder.findPath(world.get(2, 2), world.get(5, 2));
-        for (Node node : path) {
-            System.out.println(node);
-        }
 
+        // The path we expect to get:
         List<Node> expected = new ArrayList<>();
         expected.add(new SquareNode(2, 2, world));
         expected.add(new SquareNode(1, 1, world));
@@ -28,6 +30,15 @@ public class PathfinderTest {
         expected.add(new SquareNode(4, 1, world));
         expected.add(new SquareNode(5, 2, world));
 
-        assert(path.equals(expected));
+        // Check if we got the same path:
+        assertEquals("Path doesn't have the expected size", path.size(), expected.size());
+        boolean equals = true;
+        for (int i=0; i<path.size(); i++) {
+            if (path.get(i).getX() != path.get(i).getX() || path.get(i).getY() != path.get(i).getY()) {
+                equals  = false;
+                break;
+            }
+        }
+        assertTrue("Path is not the expected path.", equals);
     }
 }
